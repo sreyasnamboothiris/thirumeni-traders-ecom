2314fca38fe9e0c726bee6069c2f5df985e7e8c0
Ramprasan RP <rramprasan07@gmail.com>
2025-10-15 20:49:54 +0530

    product model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -9,4 +9,21 @@ class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
     use HasFactory;
+    protected $fillable = [
+        'sku',
+        'name',
+        'slug',
+        'category_id',
+        'brand_id',
+        'price_sell',
+        'price_buy',
+        'price_discount',
+        'stock',
+        'length_mm',
+        'width_mm',
+        'height_mm',
+        'description',
+        'thumbnail_url',
+        'status',
+    ];
 }
